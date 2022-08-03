a49012f6506547dd07780eb25d3e99a4ef19947f
Hiago Petris <hiago.petris@gmail.com>
2022-08-03 09:29:44 -0300

    First Commit

---
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -0,0 +1,30 @@
+package br.com.alura.leilao.dao;
+
+import br.com.alura.leilao.model.Usuario;
+import br.com.alura.leilao.util.JPAUtil;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioDaoTest {
+
+    private UsuarioDao dao;
+    private EntityManager em;
+
+    @Test
+    void testeBuscaDeUsuarioPeloUsername() {
+        EntityManager em = JPAUtil.getEntityManager();
+        this.dao = new UsuarioDao(em);
+
+        Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
+        em.getTransaction().begin();
+        em.persist(usuario);
+        em.getTransaction().commit();
+
+        Usuario encontrado = this.dao.buscarPorUsername(usuario.getNome());
+        assertNotNull(encontrado);
+    }
+
+}
